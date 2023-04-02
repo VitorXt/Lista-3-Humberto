2aa466b9b71a2ed0e74d671590ab394b8c98cb63
VitorHRV <62753727+VitorXt@users.noreply.github.com>
2023-04-02 16:15:42 -0300

    Add files via upload

---
+++ M1/M1/Program.cs
@@ -0,0 +1,58 @@
+﻿int[,] jureg = new int[3, 3];
+int maior = jureg[0, 0];
+int soma = 0;
+int quantidadePrimos = 0;
+
+static bool EhPrimo(int num)
+{
+    if (num < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write("Insira um valor para a posição [" + i + "," + j + "]: ");
+        jureg[i, j] = Convert.ToInt32(Console.ReadLine());
+
+        soma += jureg[i, j];
+
+        if (jureg[i, j] > maior)
+        {
+            maior = jureg[i, j];
+        }
+
+        if (EhPrimo(jureg[i, j]))
+        {
+            quantidadePrimos++;
+        }
+    }
+}
+
+Console.WriteLine("\nMatriz preenchida pelo usuário: ");
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(jureg[i, j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nO maior elemento da matriz é: " + maior);
+Console.WriteLine("\nA soma dos elementos da matriz é: " + soma);
+Console.WriteLine("\nA quantidade de números primos na matriz é: " + quantidadePrimos);
---
+++ M2/Program.cs
@@ -0,0 +1,44 @@
+﻿int[,] jureg = new int[3, 3];
+float[] medias = new float[3];
+
+for (int i = 0; i < 3; i++)
+{
+    float media = 0;
+
+
+    for (int j = 0; j < 3; j++)
+    {
+        media += jureg[j, i];
+
+        Console.Write("Insira um valor para a posição [" + i + "," + j + "]: ");
+        jureg[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    
+}
+
+for (int i = 0; i < 3; i++)
+{
+    medias[i] = (jureg[0, i] + jureg[1, i] + jureg[2, i]) / 3;
+}
+
+Console.WriteLine("\nMatriz preenchida pelo usuário: ");
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.Write(jureg[i, j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nMédias das colunas:");
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+
+    Console.WriteLine("Coluna {0}: {1}", i + 1, medias[i]);
+}
